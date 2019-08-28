34c407d1fdf368cb6e47902386949db80ad38ab2
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-08-28 12:19:55 -0500

    Finished the basic build of the component. Incorporated useEffect and axios.get request.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
 import axios from "axios";
 import ImageCard from './components/image component/image'
 
@@ -7,20 +7,22 @@ import "./App.css";
 function App() {
   const [data, setData] = useState({}); //stateful variable. Should contain the response.data object so we have access to ALL the keys inside it.
   
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=SOl08bQFOhqbyuBGL0TKGfOs2i1pbQZIgRWhpIDF')
-  .then(response =>  {
-    console.log(response.data);
-    setData(response.data)
-  })
-  .catch(function(error) {
-    console.log(error)
-  });
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=SOl08bQFOhqbyuBGL0TKGfOs2i1pbQZIgRWhpIDF')
+      .then(response =>  {
+        console.log(response.data);
+        setData(response.data)
+      })
+      .catch(function(error) {
+        console.log(error)
+      });
+  }, [])
   
   return (
     <div className="App">
       <ImageCard title = {data.title}
                  url = {data.url}
-                 description = {data.description} />
+                 description = {data.explanation} />
     </div>
   );
 }
--- src/components/image component/image.js
+++ src/components/image component/image.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function ImageCard(props) {
     console.log(props)
     return (
-    <div>
+    <div className = 'image-card'>
         <img src={props.url}></img> 
         <h2>{props.title}</h2>
         <p>{props.description}</p>
